89e6f1081dd2c34573bba4f70b77cd49c8e503eb
JeremyKoolios <jeremy.koch@xtra.co.nz>
2021-09-01 21:56:02 +1200

    Entry point created

---
+++ Episode1/GameEngine/BlankProject/Source/WinMain.cpp
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+//entry point
+//hInstance is basically a representation of the entier project that is running
+//lpCmdLine allows us to put different commands into our program at runtime
+//nCmdShow commands to choose whether to show the window at the beginning or not
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, INT nCmdShow) 
+{
+	return 0;
+}
